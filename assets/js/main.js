4509fd1f7dd52b9edc29226a254fd48f2d67c105
INFINITYVDI007\infinitydev1 <getasewmequanint@cbe.com.et>
2022-07-21 10:42:16 +0300

    adding portfolio page swiper js

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,7 +91,20 @@ modalCloses.forEach((modalClose) => {
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-var swi
+let swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    mousewheel: true,
+    keyboard: true,
+});
 /*==================== TESTIMONIAL ====================*/
 
 
