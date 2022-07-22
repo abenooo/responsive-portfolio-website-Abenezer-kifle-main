b49dddc68d2552293f2031c0d9880f0fe775cf8d
INFINITYVDI007\infinitydev1 <getasewmequanint@cbe.com.et>
2022-07-22 09:50:11 +0300

    adding testimony slide

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,7 +91,7 @@ modalCloses.forEach((modalClose) => {
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-let swiper = new Swiper('.swiper-container', {
+let swiperPortfolio = new Swiper('.swiper-container', {
     cssMode: true,
     loop: true,
     navigation: {
@@ -107,7 +107,21 @@ let swiper = new Swiper('.swiper-container', {
 });
 /*==================== TESTIMONIAL ====================*/
 
-
+// let swiperTestimonial = new Swiper('.testimonial__container', {
+//     loop: true,
+//     grabCursor:true,
+//    spaceBetween: 48,
+//        pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//         dynamicBullets: true,
+//     },
+//     breakpoints:{
+//         568:{
+//             slidesPerView:2
+//         }
+//     }
+// });
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
